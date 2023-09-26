026406402acc4dbcc688fc2694548a4566a167c5
Rosie Schofield <rschofield00@gmail.com>
2023-09-26 16:39:47 +0100

    push last changes

--- src/components/MoodPickerDemo.tsx
+++ src/components/MoodPickerDemo.tsx
@@ -5,42 +5,28 @@ function MoodPickerDemo(): JSX.Element {
     useState("confused");
 
   const handleMoodChangeToHappy = () => {
-    const nextMood = "happy";
-    queueRerenderWithNewMoodValue(nextMood);
-    console.log(
-      "😀 queued a rerender with mood as",
-      nextMood,
-      "but in this render it is still",
-      moodValueFromCurrentRender
-    );
+    queueRerenderWithNewMoodValue("happy");
   };
 
   const handleMoodChangeToConfused = () => {
-    const nextMood = "confused";
-    queueRerenderWithNewMoodValue(nextMood);
-    console.log(
-      "🤔 queued a rerender with mood as",
-      nextMood,
-      "but in this render it is still",
-      moodValueFromCurrentRender
-    );
+    queueRerenderWithNewMoodValue("confused");
   };
 
   const handleMoodChangeToSad = () => {
-    const nextMood = "sad";
-    queueRerenderWithNewMoodValue(nextMood);
-    console.log(
-      "😢 queued a rerender with mood as",
-      nextMood,
-      "but in this render it is still",
-      moodValueFromCurrentRender
-    );
+    queueRerenderWithNewMoodValue("sad");
   };
 
-  console.log(
-    "Component is rendering with a mood value of",
-    moodValueFromCurrentRender
-  );
+  const handleMoodChangeToAngry = () => {
+    queueRerenderWithNewMoodValue("angry");
+  };
+
+  const handleMoodChangeToSleepy = () => {
+    queueRerenderWithNewMoodValue("sleepy");
+  };
+
+  const handleMoodChangeToUnwell = () => {
+    queueRerenderWithNewMoodValue("unwell");
+  };
 
   return (
     <>
@@ -49,6 +35,9 @@ function MoodPickerDemo(): JSX.Element {
       <button onClick={handleMoodChangeToHappy}>😀</button>
       <button onClick={handleMoodChangeToConfused}>🤔</button>
       <button onClick={handleMoodChangeToSad}>😢</button>
+      <button onClick={handleMoodChangeToAngry}>😡</button>
+      <button onClick={handleMoodChangeToSleepy}>😴</button>
+      <button onClick={handleMoodChangeToUnwell}>🤕</button>
     </>
   );
 }
